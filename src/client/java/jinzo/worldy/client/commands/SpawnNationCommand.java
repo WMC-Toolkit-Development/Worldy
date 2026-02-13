537d7ac254cccbed0e2e32e435f0923422a58b2a
Jinzo <91829829+pernio@users.noreply.github.com>
2026-02-13 23:33:33 +0100

    Added /sn command

--- src/client/java/jinzo/worldy/client/commands/SpawnNationCommand.java
+++ src/client/java/jinzo/worldy/client/commands/SpawnNationCommand.java
@@ -7,17 +7,17 @@ import net.minecraft.client.MinecraftClient;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
-public class SpawnCommand {
+public class SpawnNationCommand {
     private static int execute(CommandContext<FabricClientCommandSource> ctx) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.player == null) return 0;
 
-        client.player.networkHandler.sendChatCommand("t spawn");
+        client.player.networkHandler.sendChatCommand("n spawn");
 
         return 1;
     }
 
     public static LiteralArgumentBuilder<FabricClientCommandSource> register() {
-        return literal("spawn").executes(SpawnCommand::execute);
+        return literal("sn").executes(SpawnNationCommand::execute);
     }
 }
---
+++ src/client/java/jinzo/worldy/client/commands/SpawnTownCommand.java
@@ -0,0 +1,27 @@
+package jinzo.worldy.client.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+
+public class SpawnTownCommand {
+    private static int execute(CommandContext<FabricClientCommandSource> ctx) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null) return 0;
+
+        client.player.networkHandler.sendChatCommand("t spawn");
+
+        return 1;
+    }
+
+    public static LiteralArgumentBuilder<FabricClientCommandSource> register() {
+        return literal("spawn").executes(SpawnTownCommand::execute);
+    }
+
+    public static LiteralArgumentBuilder<FabricClientCommandSource> registerAlias() {
+        return literal("st").executes(SpawnTownCommand::execute);
+    }
+}
